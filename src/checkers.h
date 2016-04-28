5f7a3036106ea8cb3c71f2a92175ec3816c7f2ea
Louis <artonge.c@gmail.com>
2016-04-28 15:31:44 +0200

    Remove deleted functions

--- src/checkers.h
+++ src/checkers.h
@@ -14,6 +14,3 @@ void checkAdd(ANTLR3_BASE_TREE *tree);
 void checkMult(ANTLR3_BASE_TREE *tree);
 void checkID(ANTLR3_BASE_TREE *tree);
 void checkArgs(ANTLR3_BASE_TREE *tree);
-void checkInt(ANTLR3_BASE_TREE *tree);
-void checkStr(ANTLR3_BASE_TREE *tree);
-void checkNeg(ANTLR3_BASE_TREE *tree);
